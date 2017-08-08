ea5b707116e2c01788b2db6daa4f981247dc8d50
pqml <hugo.piquemal@gmail.com>
2017-08-08 14:45:32 +0200

    Fix postcss config file

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,7 +1,5 @@
-const autoprefixer = require('autoprefixer')
-
-module.exports = function ({ file, options, env }) {
-  const config = { parser: false, plugins: [] }
-  config.plugins.push(autoprefixer({ browsers: ['last 2 versions'] }))
-  return config
+module.exports = {
+  plugins: {
+    autoprefixer: { browsers: ['last 2 versions'] }
+  }
 }
